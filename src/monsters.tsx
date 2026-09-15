a817be73d230c648563add033674a69bae66073e
Carlitzo <adrianscholl95@hotmail.com>
2026-09-15 14:36:07 +0200

    CSS and JSX-changes for monster-detail page
    Changed some of the JSX for the monster detail page and changed some CSS to make sure the detail page aligned more with the design

--- src/monsters.tsx
+++ src/monsters.tsx
@@ -49,16 +49,19 @@ export default  function MonsterContent () {
         
         const currentMonster = monsters[activeMonsterIndex];
         console.log(currentMonster);
+
+        const currentMonsterType = (currentMonster.type.charAt(0).toUpperCase() + currentMonster.type.slice(1));
+        
         const header = (
                 <div className="monsterHeader">
                                 <h2 className="monsterTitle">{currentMonster.name}</h2>
                                 <h3 className="CR">CR: {currentMonster.challenge_rating}</h3>
                                 <div className="monsterInfoBriefContainer">
-                                        <p className="monsterInfoBrief">{currentMonster.type}</p>
-                                        <p className="monsterInfoBriefSeparation">|</p>
-                                        <p className="monsterInfoBrief">{currentMonster.size.toLowerCase()}</p>
-                                        <p className="monsterInfoBriefSeparation">|</p>
-                                        <p className="monsterInfoBrief">hit-points: {currentMonster.hp}</p>
+                                        <p className="monsterInfoBrief">{currentMonsterType}</p>
+                                        <p className="monsterInfoBriefSeparation"></p>
+                                        <p className="monsterInfoBrief">{currentMonster.size}</p>
+                                        <p className="monsterInfoBriefSeparation"></p>
+                                        <p className="monsterInfoBrief">Hit-Points: {currentMonster.hp}</p>
                                 </div>
                         </div>
         );
@@ -130,21 +133,59 @@ function MonsterDetail({ monster }: MonsterDetailProps) {
                                                 <div className="actionContainer" key={index}>
                                                         <ul className="actionUl">
                                                                 <li className="actionLi" key={index}>
-                                                                        <p className="actionName">
-                                                                                {action.name}
-                                                                        </p>
-                                                                        <span className="actionDesc">{action.desc}</span>
+                                                                        <div className="actionRow">
+                                                                                <p className="actionName">
+                                                                                        {action.name}
+                                                                                </p>
+                                                                                <span className="actionDesc">{action.desc}</span>
+                                                                        </div>
                                                                 </li>
                                                         </ul>
                                                 </div>
                                         )})}
                                 </div>
-                                {monster.special_abilities && <div className="specialAbilitiesContainer">
-
-                                </div>}
-                                {monster.legendary_actions && <div className="legendaryActionsContainer">
-                                        
-                                </div>}
+                                {monster.special_abilities && (
+                                        <>
+                                        <h2 className="specialAbilites">Special Abilities:</h2>
+                                        <div className="specialAbilitiesContainer">
+                                                {monster.special_abilities.map((ability, index) => {return (
+                                                        <div className="specialAbilityContainer" key={index}>
+                                                                <ul className="specialAbilityUl">
+                                                                        <li className="specialAbilityLi" key={index}>
+                                                                                <div className="specialAbilityRow">
+                                                                                        <p className="specialAbilityName">
+                                                                                                {ability.name}
+                                                                                        </p>
+                                                                                        <span className="specialAbilityDesc">{ability.desc}</span>
+                                                                                </div>
+                                                                        </li>
+                                                                </ul>
+                                                        </div>
+                                                )})}
+                                        </div>
+                                        </>
+                                )}
+                                {monster.legendary_actions && (
+                                        <>
+                                        <h2 className="legendaryActions">Legendary Actions:</h2>
+                                        <div className="legendaryActionsContainer">
+                                                {monster.legendary_actions.map((legendary_action, index) => {return (
+                                                        <div className="legendaryActionContainer" key={index}>
+                                                                <ul className="legendaryActionUl">
+                                                                        <li className="legendaryActionLi" key={index}>
+                                                                                <div className="legendaryActionRow">
+                                                                                        <p className="legendaryActionName">
+                                                                                                {legendary_action.name}
+                                                                                        </p>
+                                                                                        <span className="legendaryActionDesc">{legendary_action.desc}</span>
+                                                                                </div>
+                                                                        </li>
+                                                                </ul>
+                                                        </div>
+                                                )})}
+                                        </div>
+                                        </>
+                                )}
                         </div>
                 </div>
         )
